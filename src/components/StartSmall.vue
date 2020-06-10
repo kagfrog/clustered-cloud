a522c2383635448a4a01fe3769501d0b3332185c
Doug Allrich <dougallrich@gmail.com>
2020-06-10 08:59:12 -0500

    feat: add start small section

--- src/components/StartSmall.vue
+++ src/components/StartSmall.vue
@@ -4,109 +4,117 @@
     tile
   >
     <v-container>
-      <!-- eslint-disable max-len -->
-      <div
-        data-v-920b2936=""
-        class="row align-end justify-space-between"
-      >
-        <div
-          data-v-920b2936=""
-          class="col-md-6 col-12"
+      <v-row class="align-end justify-space-between">
+        <v-col
+          cols="12"
+          md="6"
         >
-          <div
-            data-v-920b2936=""
-            class="text-center text-md-left"
-          >
-            <h5
-              data-v-920b2936=""
-              class="body-1 mb-4 font-weight-light"
-            >
+          <div class="text-center text-md-left">
+            <div class="body-1 mb-4 font-weight-light">
               Adopts to your needs
-            </h5><h3
-              data-v-920b2936=""
-              class="display-1 font-weight-medium mb-6"
-            >
+            </div>
+            <div class="display-1 font-weight-medium mb-6">
               Start small, grow big
-            </h3><p
-              data-v-920b2936=""
-              class="title font-weight-regular mb-6"
-            >
-              Solo developer or big dev shop? Clustered Cloud helps freelancers, startups, agencies and enterprises build better apps at any scale regardless of their size and experience.
+            </div>
+            <p class="title font-weight-regular mb-6">
+              Solo developer or big dev shop? Clustered Cloud helps
+              freelancers, startups, agencies and enterprises build better
+              apps at any scale regardless of their size and experience.
             </p>
-          </div><ul
-            data-v-920b2936=""
-            class="simple-list"
-          >
-            <li data-v-920b2936="">
-              <i
-                data-v-920b2936=""
-                aria-hidden="true"
-                class="v-icon notranslate mdi mdi-check-circle theme--light accent--text"
-              /><div data-v-920b2936="">
-                Empowers teams to work better together with shared projects
-              </div>
-            </li><li data-v-920b2936="">
-              <i
-                data-v-920b2936=""
-                aria-hidden="true"
-                class="v-icon notranslate mdi mdi-check-circle theme--light accent--text"
-              /><div data-v-920b2936="">
-                Consolidate disparate tools and services into a single unified toolchain
-              </div>
-            </li><li data-v-920b2936="">
-              <i
-                data-v-920b2936=""
-                aria-hidden="true"
-                class="v-icon notranslate mdi mdi-check-circle theme--light accent--text"
-              /><div data-v-920b2936="">
-                Get valuable insights with realtime app metrics and log aggregation
-              </div>
-            </li><li data-v-920b2936="">
-              <i
-                data-v-920b2936=""
-                aria-hidden="true"
-                class="v-icon notranslate mdi mdi-check-circle theme--light accent--text"
-              /><div data-v-920b2936="">
-                Replaces products such as GitHub, GitLab, Jira, CircleCI, Jenkins, TravisCI, Ansible, Chef, Kubernetes, AWS/GCP/Azure...
-              </div>
-            </li><li data-v-920b2936="">
-              <i
-                data-v-920b2936=""
-                aria-hidden="true"
-                class="v-icon notranslate mdi mdi-check-circle theme--light accent--text"
-              /><div data-v-920b2936="">
-                Pay only for what you use, prorated to the second
+          </div>
+          <ul class="simple-list">
+            <li
+              v-for="(item, i) in items"
+              :key="i"
+            >
+              <v-icon class="accent--text">
+                {{ icons.mdiCheckCircle }}
+              </v-icon>
+              <div>
+                {{ item.content }}
               </div>
             </li>
           </ul>
-        </div><div
-          data-v-920b2936=""
-          class="col-md-5 col-lg-4 col-12"
+        </v-col>
+        <v-col
+          cols="12"
+          md="5"
+          lg="4"
         >
-          <div
-            data-v-920b2936=""
-            class="v-responsive v-image mx-auto"
-            style="max-width: 400px;"
-          >
-            <div
-              class="v-responsive__sizer"
-              style="padding-bottom: 82.2917%;"
-            /><div
-              class="v-image__image v-image__image--cover"
-              style="background-image: url(&quot;https://clustered.com/img/grow-accent.6ccc08f0.svg&quot;); background-position: center center;"
-            /><div
-              class="v-responsive__content"
-              style="width: 864px;"
-            />
-          </div>
-        </div>
-      </div>
+          <v-img
+            :src="require('../../public/img/svg/grow-accent.svg')"
+            class="mx-auto"
+            max-width="400"
+          />
+        </v-col>
+      </v-row>
     </v-container>
   </v-sheet>
 </template>
-
 <script>
-export default {
+import { mdiCheckCircle } from '@mdi/js';
 
+export default {
+  data () {
+    return {
+      icons: { mdiCheckCircle },
+      items: [
+        {
+          content: `
+            Empowers teams to work better together with shared projects
+          `,
+        },
+        {
+          content: `
+            Consolidate disparate tools and services into a single unified
+            toolchain
+          `,
+        },
+        {
+          content: `
+            Get valuable insights with realtime app metrics and log aggregation
+          `,
+        },
+        {
+          content: `
+            Replaces products such as GitHub, GitLab, Jira, CircleCI,
+            Jenkins, TravisCI, Ansible, Chef, Kubernetes, AWS/GCP/Azure...
+          `,
+        },
+        {
+          content: `
+            Pay only for what you use, prorated to the second
+          `,
+        },
+      ],
+    };
+  },
 };
 </script>
+
+<style scoped>
+.simple-list {
+  list-style: none;
+  margin-top: 32px;
+}
+
+.simple-list li {
+  display: flex;
+  -webkit-box-align: start;
+  -ms-flex-align: start;
+  align-items: flex-start;
+}
+
+.simple-list li:not(:last-child) {
+  padding-bottom: 16px;
+}
+
+.simple-list li > :first-child {
+  -webkit-box-flex: 0;
+  -ms-flex-positive: 0;
+  flex-grow: 0;
+  -ms-flex-negative: 0;
+  flex-shrink: 0;
+  margin-right: 24px;
+}
+</style>
