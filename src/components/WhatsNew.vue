4c943bb0d3ffa7607054dcc38a30334c1b936438
Doug Allrich <dougallrich@gmail.com>
2020-06-10 16:00:22 -0500

    feat: add what is new section

--- src/components/WhatsNew.vue
+++ src/components/WhatsNew.vue
@@ -2,161 +2,171 @@
   <v-sheet
     class="page-section"
     tile
+    tag="section"
   >
     <v-container>
-      <!-- eslint-disable max-len -->
-      <h3
-        data-v-920b2936=""
-        class="text-center display-2 font-weight-medium mb-6"
-      >
+      <h3 class="text-center display-2 font-weight-medium mb-6">
         What's new
       </h3>
-      <div
-        data-v-920b2936=""
-        class="row py-12 flex-md-nowrap justify-md-space-around"
-      >
-        <div
-          data-v-920b2936=""
-          class="flex-shrink-1 col-sm-auto col-12"
+      <v-row class="py-12 flex-md-nowrap justify-md-space-around">
+        <template
+          v-for="(item, i) in items"
         >
-          <a
-            data-v-920b2936=""
-            href="/blog/zero-downtime-deployments-with-git-and-cicd"
-            class="newsbox v-card v-card--flat v-card--link v-sheet theme--light"
-            tabindex="0"
-          ><figure
-            data-v-920b2936=""
-            class="post-image"
-          ><div
-            data-v-920b2936=""
-            class="post-image__inner v-sheet v-sheet--tile theme--light blue lighten-5"
-          ><i
-            data-v-920b2936=""
-            aria-hidden="true"
-            class="v-icon notranslate mdi mdi-image theme--light"
-            style="font-size: 64px;"
-          /></div></figure><div
-            data-v-920b2936=""
-            class="v-card__subtitle pb-2"
-          >Blog</div><div
-            data-v-920b2936=""
-            class="v-card__title headline newsbox-headline"
-          >Zero-downtime deployments with Git and CI/CD</div><div
-            data-v-920b2936=""
-            class="v-card__text newsbox-text"
-          >Simplify your development workflow with atomic deployments and automated build pipelines.</div><div
-            data-v-920b2936=""
-            class="v-card__actions"
-          ><button
-            data-v-920b2936=""
-            type="button"
-            class="nudge-icon v-btn v-btn--flat v-btn--text theme--light v-size--default primary--text"
-          ><span class="v-btn__content"> Learn more <i
-            data-v-920b2936=""
-            aria-hidden="true"
-            class="v-icon notranslate v-icon--right mdi mdi-arrow-right theme--light"
-          /></span></button></div></a>
-        </div><hr
-          data-v-920b2936=""
-          role="separator"
-          aria-orientation="vertical"
-          class="hidden-sm-and-down v-divider v-divider--vertical theme--light"
-        ><div
-          data-v-920b2936=""
-          class="flex-shrink-1 col-sm-auto col-12"
-        >
-          <a
-            data-v-920b2936=""
-            href="/webinars/"
-            class="newsbox v-card v-card--flat v-card--link v-sheet theme--light"
-            tabindex="0"
-          ><figure
-            data-v-920b2936=""
-            class="post-image"
-          ><div
-            data-v-920b2936=""
-            class="post-image__inner v-sheet v-sheet--tile theme--light blue lighten-5"
-          ><i
-            data-v-920b2936=""
-            aria-hidden="true"
-            class="v-icon notranslate mdi mdi-play theme--light"
-            style="font-size: 64px;"
-          /></div></figure><div
-            data-v-920b2936=""
-            class="v-card__subtitle pb-2"
-          >Webinar</div><div
-            data-v-920b2936=""
-            class="v-card__title headline newsbox-headline"
-          >How to deploy a highly available Magento cluster in 3 simple steps</div><div
-            data-v-920b2936=""
-            class="v-card__text newsbox-text"
-          >Learn how to eliminate slowness and downtime with self-healing containers and horizontal scaling.</div><div
-            data-v-920b2936=""
-            class="v-card__actions"
-          ><button
-            data-v-920b2936=""
-            type="button"
-            class="nudge-icon v-btn v-btn--flat v-btn--text theme--light v-size--default primary--text"
-          ><span class="v-btn__content"> Learn more <i
-            data-v-920b2936=""
-            aria-hidden="true"
-            class="v-icon notranslate v-icon--right mdi mdi-arrow-right theme--light"
-          /></span></button></div></a>
-        </div><hr
-          data-v-920b2936=""
-          role="separator"
-          aria-orientation="vertical"
-          class="hidden-sm-and-down v-divider v-divider--vertical theme--light"
-        ><div
-          data-v-920b2936=""
-          class="flex-shrink-1 col-sm-auto col-12"
-        >
-          <a
-            data-v-920b2936=""
-            href="/blog/introducing-clustered-cloud"
-            class="newsbox v-card v-card--flat v-card--link v-sheet theme--light"
-            tabindex="0"
-          ><figure
-            data-v-920b2936=""
-            class="post-image"
-          ><div
-            data-v-920b2936=""
-            class="post-image__inner v-sheet v-sheet--tile theme--light blue lighten-5"
-          ><i
-            data-v-920b2936=""
-            aria-hidden="true"
-            class="v-icon notranslate mdi mdi-image theme--light"
-            style="font-size: 64px;"
-          /></div></figure><div
-            data-v-920b2936=""
-            class="v-card__subtitle pb-2"
-          >Blog</div><div
-            data-v-920b2936=""
-            class="v-card__title headline newsbox-headline"
-          >Introducing Clustered Cloud: a new way to build and manage apps</div><div
-            data-v-920b2936=""
-            class="v-card__text newsbox-text"
-          >Clustered Cloud brings DevOps automation and  container hosting to everyone in a
-            single platform.</div><div
-            data-v-920b2936=""
-            class="v-card__actions"
-          ><button
-            data-v-920b2936=""
-            type="button"
-            class="nudge-icon v-btn v-btn--flat v-btn--text theme--light v-size--default primary--text"
-          ><span class="v-btn__content"> Learn more <i
-            data-v-920b2936=""
-            aria-hidden="true"
-            class="v-icon notranslate v-icon--right mdi mdi-arrow-right theme--light"
-          /></span></button></div></a>
-        </div><!---->
-      </div>
+          <v-col
+            :key="i"
+            class="flex-shrink-1"
+            cols="12"
+            sm="auto"
+          >
+            <v-card
+              :to="item.path"
+              flat
+              class="newsbox"
+            >
+              <figure class="post-image">
+                <v-sheet
+                  tile
+                  class="post-image__inner blue lighten-5"
+                >
+                  <v-icon size="64">
+                    {{ item.icon }}
+                  </v-icon>
+                </v-sheet>
+              </figure>
+              <v-card-subtitle class="pb-2">
+                {{ item.topic }}
+              </v-card-subtitle>
+              <v-card-title class="headline newsbox-headline">
+                {{ item.header }}
+              </v-card-title>
+              <v-card-text class="newsbox-text">
+                {{ item.content }}
+              </v-card-text>
+              <v-card-actions>
+                <v-btn
+                  text
+                  class="nudge-icon"
+                  color="primary"
+                >
+                  Learn more
+                  <v-icon right>
+                    {{ icons.mdiArrowRight }}
+                  </v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-divider
+            v-if="item.divider"
+            :key="i"
+            vertical
+            class="hidden-sm-and-down"
+          />
+        </template>
+      </v-row>
     </v-container>
   </v-sheet>
 </template>
 
 <script>
-export default {
+import { mdiArrowRight, mdiImage, mdiPlay } from '@mdi/js';
 
+export default {
+  data () {
+    return {
+      icons: { mdiArrowRight },
+      items: [
+        {
+          topic: 'Blog',
+          icon: mdiImage,
+          divider: true,
+          path: '/blog',
+          header: 'Zero-downtime deployments with Git and CI/CD',
+          content: `
+            Simplify your development workflow with atomic deployments and
+            automated build pipelines.
+          `,
+        },
+        {
+          topic: 'Webinar',
+          icon: mdiPlay,
+          divider: true,
+          path: '/webinars',
+          header: `
+            How to deploy a highly available Magento cluster in 3
+            simple steps
+          `,
+          content: `
+            Learn how to eliminate slowness and downtime with self-healing
+            containers and horizontal scaling.
+          `,
+        },
+        {
+          topic: 'Blog',
+          icon: mdiImage,
+          divider: false,
+          path: '/blog',
+          header: `
+            Introducing Clustered Cloud: a new way to build and
+            manage apps
+          `,
+          content: `
+            Clustered Cloud brings DevOps automation and  container hosting
+            to everyone in a single platform.
+          `,
+        },
+      ],
+    };
+  },
 };
 </script>
+
+<style scoped>
+.nudge-icon:hover .v-icon {
+  transform: translateX(4px);
+}
+
+.post-image {
+  height: 200px;
+  overflow: hidden;
+}
+
+.post-image__inner {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform-origin: 60% 0;
+  transition: transform 0.35s ease-in-out;
+}
+
+.newsbox {
+  height: 100%;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  flex-direction: column;
+}
+
+.newsbox-headline {
+  font-weight: 700 !important;
+  padding-top: 0;
+  word-break: normal;
+}
+
+.newsbox:hover .newsbox-headline {
+  opacity: 0.7;
+}
+
+.newsbox-text {
+  font-size: 1rem;
+  color: #000000de !important;
+}
+
+@media only screen and (min-width: 600px) {
+  .newsbox {
+    min-width: 200px;
+    max-width: 300px;
+  }
+}
+</style>
