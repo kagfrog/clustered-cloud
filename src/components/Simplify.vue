f20cf1d6c8024d7961f320e1417f0f49ccde5c2a
Doug Allrich <dougallrich@gmail.com>
2020-06-10 09:32:51 -0500

    feat: add simplify section

--- src/components/Simplify.vue
+++ src/components/Simplify.vue
@@ -5,100 +5,120 @@
   >
     <v-container>
       <!-- eslint-disable max-len -->
-      <div
-        data-v-920b2936=""
-        class="row align-center justify-space-between"
-      >
-        <div
-          data-v-920b2936=""
-          class="col-md-5 col-lg-4 col-12"
+      <v-row class="align-center justify-space-between">
+        <v-col
+          cols="12"
+          md="5"
+          lg="4"
         >
+          <v-img
+            :src="require('../../public/img/svg/team-accent.svg')"
+            class="mx-auto"
+            max-width="400"
+          />
+        </v-col>
+        <div class="text-center text-md-left col-md-6 col-12">
           <div
-            data-v-920b2936=""
-            class="v-responsive v-image mx-auto"
-            style="max-width: 400px;"
-          >
-            <div
-              class="v-responsive__sizer"
-              style="padding-bottom: 74.6416%;"
-            /><div
-              class="v-image__image v-image__image--cover"
-              style="background-image: url(&quot;https://clustered.com/img/team-accent.fbba2c09.svg&quot;); background-position: center center;"
-            /><div
-              class="v-responsive__content"
-              style="width: 1116px;"
-            />
-          </div>
-        </div><div
-          data-v-920b2936=""
-          class="text-center text-md-left col-md-6 col-12"
-        >
-          <h5
             id="cloud-migration"
-            data-v-920b2936=""
             class="body-1 mb-4 font-weight-light"
           >
             Remove cloud barriers
-          </h5><h3
-            data-v-920b2936=""
-            class="display-1 font-weight-medium mb-6"
-          >
+          </div>
+          <div class="display-1 font-weight-medium mb-6">
             Simplify migration to the cloud
-          </h3><p
-            data-v-920b2936=""
-            class="title font-weight-regular mb-6"
-          >
-            Unfortunately, most applications were not built to be <a
-              data-v-920b2936=""
+          </div>
+          <p class="title font-weight-regular mb-6">
+            Unfortunately, most applications were not built to be
+            <a
               href="https://12factor.net/"
               target="_blank"
-            >cloud-ready</a>. Clustered let you migrate your existing apps with minimal effort so you can take full advantage of the cloud.
-          </p><ul
-            data-v-920b2936=""
-            class="simple-list"
-          >
-            <li data-v-920b2936="">
-              <i
-                data-v-920b2936=""
-                aria-hidden="true"
-                class="v-icon notranslate mdi mdi-check-circle theme--light accent--text"
-              /><div data-v-920b2936="">
-                Deploy any kind of application, including complex CMS and ecommerce platforms such as Wordpress and Magento
-              </div>
-            </li><li data-v-920b2936="">
-              <i
-                data-v-920b2936=""
-                aria-hidden="true"
-                class="v-icon notranslate mdi mdi-check-circle theme--light accent--text"
-              /><div data-v-920b2936="">
-                Create scalable clusters of <strong data-v-920b2936="">stateless and stateful</strong> apps by using the built-in, container-native storage volumes
-              </div>
-            </li><li data-v-920b2936="">
-              <i
-                data-v-920b2936=""
-                aria-hidden="true"
-                class="v-icon notranslate mdi mdi-check-circle theme--light accent--text"
-              /><div data-v-920b2936="">
-                Supports apps that normally doesn't work in a containerized environment
-              </div>
-            </li><li data-v-920b2936="">
-              <i
-                data-v-920b2936=""
-                aria-hidden="true"
-                class="v-icon notranslate mdi mdi-check-circle theme--light accent--text"
-              /><div data-v-920b2936="">
-                Works with your existing application code, frameworks and integrations
+            >
+              cloud-ready
+            </a>.
+            Clustered let you migrate your existing apps with minimal
+            effort so you can take full advantage of the cloud.
+          </p>
+          <ul class="simple-list">
+            <li
+              v-for="(item, i) in items"
+              :key="i"
+            >
+              <v-icon class="accent--text">
+                {{ icons.mdiCheckCircle }}
+              </v-icon>
+              <div>
+                {{ item.content }}
               </div>
             </li>
           </ul>
         </div>
-      </div>
+      </v-row>
     </v-container>
   </v-sheet>
 </template>
 
 <script>
-export default {
+import { mdiCheckCircle } from '@mdi/js';
 
+export default {
+  data () {
+    return {
+      icons: { mdiCheckCircle },
+      items: [
+        {
+          content: `
+            Deploy any kind of application, including complex CMS and
+            ecommerce platforms such as Wordpress and Magento
+          `,
+        },
+        {
+          content: `
+            Create scalable clusters of <strong>stateless and stateful</strong>
+            apps by using the built-in, container-native storage volumes
+            toolchain
+          `,
+        },
+        {
+          content: `
+            Supports apps that normally doesn't work in a containerized
+            environment
+          `,
+        },
+        {
+          content: `
+            Works with your existing application code, frameworks and
+            integrations
+          `,
+        },
+      ],
+    };
+  },
 };
 </script>
+
+<style scoped>
+.simple-list {
+  list-style: none;
+  margin-top: 32px;
+}
+
+.simple-list li {
+  display: flex;
+  -webkit-box-align: start;
+  -ms-flex-align: start;
+  align-items: flex-start;
+}
+
+.simple-list li:not(:last-child) {
+  padding-bottom: 16px;
+}
+
+.simple-list li > :first-child {
+  -webkit-box-flex: 0;
+  -ms-flex-positive: 0;
+  flex-grow: 0;
+  -ms-flex-negative: 0;
+  flex-shrink: 0;
+  margin-right: 24px;
+}
+</style>
