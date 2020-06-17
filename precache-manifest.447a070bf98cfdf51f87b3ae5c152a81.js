cc384b4a0bb54954841bc13d6650f301ee041fc5
Doug Allrich <dougallrich@gmail.com>
2020-06-16 21:36:56 -0500

    deploy

---
+++ precache-manifest.447a070bf98cfdf51f87b3ae5c152a81.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f208a684cdb81685b1d1",
+    "url": "/clustered-cloud/css/app.b0b6aa88.css"
+  },
+  {
+    "revision": "8b941562ed087fede5d9",
+    "url": "/clustered-cloud/css/chunk-vendors.7e4bddea.css"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/clustered-cloud/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/clustered-cloud/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/clustered-cloud/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/clustered-cloud/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/clustered-cloud/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/clustered-cloud/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/clustered-cloud/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/clustered-cloud/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/clustered-cloud/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/clustered-cloud/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/clustered-cloud/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/clustered-cloud/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/clustered-cloud/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/clustered-cloud/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/clustered-cloud/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/clustered-cloud/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/clustered-cloud/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/clustered-cloud/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/clustered-cloud/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/clustered-cloud/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/clustered-cloud/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/clustered-cloud/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/clustered-cloud/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/clustered-cloud/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "c900be963ea596d546429c1d62ca1386",
+    "url": "/clustered-cloud/img/clustered-dark.c900be96.svg"
+  },
+  {
+    "revision": "184c83f6e3c70757fe48469b68dcf99a",
+    "url": "/clustered-cloud/img/clustered-mini.184c83f6.svg"
+  },
+  {
+    "revision": "294ce218eb3785ae3dc0a91308f891d5",
+    "url": "/clustered-cloud/img/clustered.294ce218.svg"
+  },
+  {
+    "revision": "c589c11415f480b2150abe2ba57301fd",
+    "url": "/clustered-cloud/img/golang.c589c114.svg"
+  },
+  {
+    "revision": "6ccc08f08d629e32c4023aa054b65d85",
+    "url": "/clustered-cloud/img/grow-accent.6ccc08f0.svg"
+  },
+  {
+    "revision": "525f317fe9e90927edf7694d88ce42c8",
+    "url": "/clustered-cloud/img/html5.525f317f.svg"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/clustered-cloud/img/javascript.dd60b11e.svg"
+  },
+  {
+    "revision": "d52287738b55788ea70175d566102073",
+    "url": "/clustered-cloud/img/laravel.d5228773.svg"
+  },
+  {
+    "revision": "484c1936ab79fb26dadfcfc1c1e05316",
+    "url": "/clustered-cloud/img/magento.484c1936.svg"
+  },
+  {
+    "revision": "2b4fe3429f6ae35533d89d5e48d1f6fb",
+    "url": "/clustered-cloud/img/nodejs.2b4fe342.svg"
+  },
+  {
+    "revision": "8aaaf60a404932b6504a985c0980c9c6",
+    "url": "/clustered-cloud/img/php.8aaaf60a.svg"
+  },
+  {
+    "revision": "8e14966df3557c6165f21d86e74562bf",
+    "url": "/clustered-cloud/img/producthunt.8e14966d.svg"
+  },
+  {
+    "revision": "7f9deb42fa8fc597ef09b2eb81401408",
+    "url": "/clustered-cloud/img/python.7f9deb42.svg"
+  },
+  {
+    "revision": "c900be963ea596d546429c1d62ca1386",
+    "url": "/clustered-cloud/img/svg/clustered-dark.svg"
+  },
+  {
+    "revision": "184c83f6e3c70757fe48469b68dcf99a",
+    "url": "/clustered-cloud/img/svg/clustered-mini.svg"
+  },
+  {
+    "revision": "294ce218eb3785ae3dc0a91308f891d5",
+    "url": "/clustered-cloud/img/svg/clustered.svg"
+  },
+  {
+    "revision": "c589c11415f480b2150abe2ba57301fd",
+    "url": "/clustered-cloud/img/svg/golang.svg"
+  },
+  {
+    "revision": "6ccc08f08d629e32c4023aa054b65d85",
+    "url": "/clustered-cloud/img/svg/grow-accent.svg"
+  },
+  {
+    "revision": "525f317fe9e90927edf7694d88ce42c8",
+    "url": "/clustered-cloud/img/svg/html5.svg"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/clustered-cloud/img/svg/javascript.svg"
+  },
+  {
+    "revision": "d52287738b55788ea70175d566102073",
+    "url": "/clustered-cloud/img/svg/laravel.svg"
+  },
+  {
+    "revision": "484c1936ab79fb26dadfcfc1c1e05316",
+    "url": "/clustered-cloud/img/svg/magento.svg"
+  },
+  {
+    "revision": "2b4fe3429f6ae35533d89d5e48d1f6fb",
+    "url": "/clustered-cloud/img/svg/nodejs.svg"
+  },
+  {
+    "revision": "8aaaf60a404932b6504a985c0980c9c6",
+    "url": "/clustered-cloud/img/svg/php.svg"
+  },
+  {
+    "revision": "8e14966df3557c6165f21d86e74562bf",
+    "url": "/clustered-cloud/img/svg/producthunt.svg"
+  },
+  {
+    "revision": "7f9deb42fa8fc597ef09b2eb81401408",
+    "url": "/clustered-cloud/img/svg/python.svg"
+  },
+  {
+    "revision": "fbba2c0990d05fd317e0103c91ac38e7",
+    "url": "/clustered-cloud/img/svg/team-accent.svg"
+  },
+  {
+    "revision": "eb500929a8b7b4dd8978e0fc1322641d",
+    "url": "/clustered-cloud/img/svg/tech-stacks.svg"
+  },
+  {
+    "revision": "73dfbafab06e41d443d31440366c23d4",
+    "url": "/clustered-cloud/img/svg/vuejs.svg"
+  },
+  {
+    "revision": "985c62ba9f777afa99f82b3e43fe5935",
+    "url": "/clustered-cloud/img/svg/wordpress.svg"
+  },
+  {
+    "revision": "fbba2c0990d05fd317e0103c91ac38e7",
+    "url": "/clustered-cloud/img/team-accent.fbba2c09.svg"
+  },
+  {
+    "revision": "eb500929a8b7b4dd8978e0fc1322641d",
+    "url": "/clustered-cloud/img/tech-stacks.eb500929.svg"
+  },
+  {
+    "revision": "73dfbafab06e41d443d31440366c23d4",
+    "url": "/clustered-cloud/img/vuejs.73dfbafa.svg"
+  },
+  {
+    "revision": "5634505ce59f49f8dfb852982483a40d",
+    "url": "/clustered-cloud/img/wistia.5634505c.png"
+  },
+  {
+    "revision": "5634505ce59f49f8dfb852982483a40d",
+    "url": "/clustered-cloud/img/wistia.png"
+  },
+  {
+    "revision": "985c62ba9f777afa99f82b3e43fe5935",
+    "url": "/clustered-cloud/img/wordpress.985c62ba.svg"
+  },
+  {
+    "revision": "62a47d8d1e364238ffa7ee3ad8eb7e1e",
+    "url": "/clustered-cloud/index.html"
+  },
+  {
+    "revision": "f208a684cdb81685b1d1",
+    "url": "/clustered-cloud/js/app.11905dcd.js"
+  },
+  {
+    "revision": "8b941562ed087fede5d9",
+    "url": "/clustered-cloud/js/chunk-vendors.07da9351.js"
+  },
+  {
+    "revision": "de83da4954f67440b0ec4e609ac01a99",
+    "url": "/clustered-cloud/manifest.json"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,32 @@
+importScripts("/clustered-cloud/precache-manifest.447a070bf98cfdf51f87b3ae5c152a81.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-undef, no-underscore-dangle, no-restricted-globals */
+
+self.addEventListener('message', (e) => {
+  if (!e.data) {
+    return;
+  }
+
+  switch (e.data) {
+    case 'skipWaiting':
+      self.skipWaiting();
+      break;
+    default:
+      // NOOP
+      break;
+  }
+});
+
+// Cleans up old precached data.
+workbox.precaching.cleanupOutdatedCaches();
+
+// Prefixes the cache name in the Dev Tools Cache Storage viewer.
+workbox.core.setCacheNameDetails({ prefix: 'clustered-cloud' });
+
+// Throws promise error in dev tools console if placed inside switch() above
+// as 'self.clients.claim()'. Works quite nicely here, however.
+workbox.core.clientsClaim();
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
